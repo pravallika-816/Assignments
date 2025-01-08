2ae89686ab35d1be326b475f58d0505389bb7907
pravallika-816 <135851195+pravallika-816@users.noreply.github.com>
2025-01-08 11:51:36 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,14 @@
+//function demo(){
+//    console.log("Hello i am function demo()");
+//}
+//demo();
+
+function add(a,b){
+    console.log(a+b);
+}
+add(5,6);
+add(7,8);
+add(3,4);
+add(5,6);
+add(5,4);
+add(10,30);
---
+++ loopingstatments.js
@@ -0,0 +1,20 @@
+//FOR LOOP//
+//let fact=1;
+//for (let j=5; j>=1; j--){
+//    fact=fact*j;
+//}
+//console.log(fact);
+
+//WHILE LOOP//
+let i=1;
+//while(i<=5){
+//    console.log("Good morning")
+//    i+=1;
+//}
+
+//DO - WHILE//
+//do{
+//    console.log("Wake Up");
+//    i++;
+//}
+//while(i<=5);
