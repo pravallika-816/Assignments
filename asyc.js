1910b51306b17e417c6b732ebdb7bf75347d6357
pravallika-816 <135851195+pravallika-816@users.noreply.github.com>
2025-01-23 10:35:19 +0530

    asyc.js

--- asyc.js
+++ asyc.js
@@ -6,9 +6,39 @@
 //console.log(40)
 //setInterval(()=>{
   //  document.writeln('MRU')},1500)
-
-//API fetching
-function fetchUsers(){
+//!Promise
+/*  let p1= new Promise((resolve, reject)=>{});
+  console.log(p1)
+  
+  let p2= new Promise((resolve, reject)=>{
+      resolve("Success");
+  });
+  console.log(p2)
+  p2.then((response)=>{
+      console.log(response)
+  }).catch((error)=>{
+      console.log(error)
+  }).finally(()=>{
+      console.log("finally printing for both!")
+  })
+  
+  let p3= new Promise((resolve, reject)=>{
+      reject("Failures")
+  });
+  console.log(p3);
+  p3.catch((error)=>{
+      console.log(error)
+  }).finally(()=>{
+      console.log("finally printing for both!")
+  })*/
+  
+  
+  // handling promise with: 
+  // then():will execute if no errors
+  // catch():will execute if errors are found 
+  // finally(): will execute any way
+//!API fetching
+/*function fetchUsers(){
     let res = window.fetch("https://jsonplaceholder.typicode.com/users")
     //console.log(res);
     res.then(val => {
@@ -31,7 +61,26 @@ function fetchUsers(){
             })
         });
     }).catch(err => {
-        console.log(err);
-    })
-    }
-    fetchUsers();
+    console.log(err);
+})
+}
+//!async
+fetchUsers();*/
+/*let p=new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("promise is successfully completing")
+    },3000)
+})
+async function demo(){
+    console.log('Start')
+    let x=await p
+    console.log(x)
+    console.log('End')
+}
+demo()*/
+async function fetchUsers() {
+    let response=await fetch ("https://jsonplaceholder.typicode.com/users");
+    let data=await response.json();
+    console.log(data);   
+}
+fetchUsers();
